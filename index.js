4b2442fa484dfe29bbf3d409b8e51cdcb4f7be5a
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-10-25 09:20:40 -0500

    routing with express

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('hi there¡')
 })
 
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Pc-Gamer',
+    price: 4000
+  })
+})
+
 app.listen(port, () => {
   console.log(`el server esta corriendo en ele puerto ${port}`)
 })
