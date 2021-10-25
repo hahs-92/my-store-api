2410c3e6ca550648b98eca45c9275604d1e683aa
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-10-25 09:08:21 -0500

    install express -firts server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("my api")
+const express = require('express')
+
+const app = express()
+const port = 3005;
+
+app.get('/', (req, res) => {
+  res.send('hi there¡')
+})
+
+app.listen(port, () => {
+  console.log(`el server esta corriendo en ele puerto ${port}`)
+})
