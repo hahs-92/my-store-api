5416be5cfae10443c6f0a52d7678ddad44c7724a
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-10-25 11:19:21 -0500

    params query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+//API FAKE
+const faker = require('faker')
 
 const app = express()
 const port = 3005;
@@ -40,9 +42,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
+  const products = []
+  const { size } = req.query
+  const limit = size || 10
+
+  for(let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+
   res.json({
-    data: data
+    data: products,
+    count: products.length
   })
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('hi, Los endpoints especificos deben declararsen antes de los endpoints dinamicos. ')
 })
 
 app.get('/products/:id', (req, res) => {
@@ -81,6 +100,19 @@ app.get('/categories/:id_category/products/:id_product', (req, res) => {
     id_category: id_category,
     id_product: id_product
   })
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+
+  if(limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('no hay query')
+  }
 })
 
 
